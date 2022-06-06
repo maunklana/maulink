8acf993df877a83a7a2aadf1d38d1da8b5a651ac
chickenin <92208420+chickenin@users.noreply.github.com>
2022-06-06 22:43:10 +0700

    ok

--- asepdila/js/functions.js
+++ asepdila/js/functions.js
@@ -55,25 +55,134 @@ let showEnvelope = function(){
 		imageHeight: 300,
 		imageAlt: 'Asep Maulana Nuriman (Maunklana) & Nabila Dea Santika (Dila)'
 	}).then((result) => {
-		showInvitation();		
-		Swal.fire({
-			title: '',
-			html: '<h1><i class="bi bi-volume-up-fill"></i></h1>Nyalakan backsound?',
-			confirmButtonColor: '#991188', //Warna kesukaan Nabila
-			showCancelButton: true,
-			confirmButtonText: 'Ya, nyalakan',
-			cancelButtonText: 'Tidak',
-			reverseButtons: true,
-		}).then((result) => {					
-			if (result.isConfirmed) {
-				players.audio.play();
-			}
-		})
+		showInvitation();
+		if(typeof kepada == 'undefined'){
+			Swal.fire({
+				html: '<span style="color:white;"><h1><i class="bi bi-person-bounding-box"></i></h1>Masukkan nama kamu!</span>',
+				input: 'text',
+				inputAttributes: {
+					autocapitalize: 'off'
+				},
+				color: "white",
+				showCancelButton: false,
+				confirmButtonText: 'Lanjut',
+				confirmButtonColor: '#991188', //Warna kesukaan Nabila
+				background: 'transparent',
+				backdrop: `
+				linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+				url("img/bg-landing.jpg")
+				no-repeat center/auto 100%
+				`,
+				allowOutsideClick: false,
+				allowEscapeKey: false,
+				allowEnterKey: false
+			}).then((result) => {
+				console.log(result);
+				if (result.isConfirmed) {
+					kepada = result.value;
+					if(typeof kepada !== 'undefined'){
+						addURLParameter('to', kepada);
+						generateQrBukuTamu();						
+						Swal.fire({
+							title: '',
+							html: '<h1><i class="bi bi-volume-up-fill"></i></h1>Nyalakan backsound?',
+							confirmButtonColor: '#991188', //Warna kesukaan Nabila
+							showCancelButton: true,
+							confirmButtonText: 'Ya, nyalakan',
+							cancelButtonText: 'Tidak',
+							reverseButtons: true,
+						}).then((result) => {					
+							if (result.isConfirmed) {
+								players.audio.play();
+							}
+						});
+					}
+				}
+			});
+		}else{
+			Swal.fire({
+				title: '',
+				html: '<h1><i class="bi bi-volume-up-fill"></i></h1>Nyalakan backsound?',
+				confirmButtonColor: '#991188', //Warna kesukaan Nabila
+				showCancelButton: true,
+				confirmButtonText: 'Ya, nyalakan',
+				cancelButtonText: 'Tidak',
+				reverseButtons: true,
+			}).then((result) => {					
+				if (result.isConfirmed) {
+					players.audio.play();
+				}
+			})
+		}
 	})
 }
 let showInvitation = function(){
 	$('.xhidden').each(function() {
 		$(this).addClass('animate__animated animate__slideInUp');
 		$(this).css('visibility', 'visible');
+	});
+}
+
+function removeURLParameter(url, parameter) {
+	//prefer to use l.search if you have a location/link object
+	var urlparts = url.split('?');
+	if (urlparts.length >= 2) {
+
+		var prefix = encodeURIComponent(parameter) + '=';
+		var pars = urlparts[1].split(/[&;]/g);
+
+		//reverse iteration as may be destructive
+		for (var i = pars.length; i-- > 0;) {    
+			//idiom for string.startsWith
+			if (pars[i].lastIndexOf(prefix, 0) !== -1) {  
+				pars.splice(i, 1);
+			}
+		}
+
+		url =  urlparts[0] + (pars.length > 0 ? '?' + pars.join('&') : '');
+	}
+	
+	window.history.pushState('', '', url);
+}
+
+function addURLParameter(key, value) {	
+    key = encodeURIComponent(key);
+    value = encodeURIComponent(value);
+
+    // kvp looks like ['key1=value1', 'key2=value2', ...]
+    var kvp = document.location.search.substr(1).split('&');
+    let i=0;
+
+    for(; i<kvp.length; i++){
+        if (kvp[i].startsWith(key + '=')) {
+            let pair = kvp[i].split('=');
+            pair[1] = value;
+            kvp[i] = pair.join('=');
+            break;
+        }
+    }
+
+    if(i >= kvp.length){
+        kvp[kvp.length] = [key,value].join('=');
+    }
+
+    // can return this or...
+    let params = kvp.join('&');
+    params = params.replace(/^\&+|\&+$/g, '');
+
+    // reload page with new params
+	window.history.pushState('', '', document.location.pathname+'?'+params);
+}
+
+let generateQrBukuTamu = function(){
+	$('#qrbukutamu').kjua({
+		text: 'https://maunklana.github.io/bukutamu/?addattender='+encodeURIComponent(btoa(kepada)),
+		size: 200,
+		render: 'svg',
+		crisp: true,
+		minVersion: 4,
+		quiet: 2,
+		ecLevel: 'H',
+		back: '#f5f5e9'
 	});
 }
