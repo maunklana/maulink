ee9b8af1d00ac612f1708d05da776917cdb3baa6
Asep Maulana Nuriman <bokuwa.macan@gmail.com>
2022-06-21 18:26:19 +0700

    sd

---
+++ asepdila/config.js
@@ -0,0 +1,10 @@
+let theConfigs = {
+	"groom" : "Asep Maulana Nuriman",
+	"groom_nickname" : "Maunklana",
+	"bride" : "Nabila Dea Santika"
+	"bride_nickname" : "Dila"
+};
+
+const createConfigs = function(){
+	console.log(theConfigs);
+}
